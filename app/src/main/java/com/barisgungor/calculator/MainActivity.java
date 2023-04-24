56a24ff509a8b60bc5f20e9a247219a3118d5c55
Eren Celik <63004133+eren-celik@users.noreply.github.com>
2023-04-24 22:26:56 +0300

    applyed lint & refactored

--- app/src/main/java/com/barisgungor/calculator/MainActivity.java
+++ app/src/main/java/com/barisgungor/calculator/MainActivity.java
@@ -2,64 +2,71 @@ package com.barisgungor.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    EditText number1Text;
-    EditText number2Text;
-    TextView resultText;
+    private EditText number1Text;
+    private EditText number2Text;
+    private TextView resultText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-          number1Text = findViewById(R.id.number1Text);
-          number2Text = findViewById(R.id.number2Text);
-          resultText = findViewById(R.id.resultText);
+        number1Text = findViewById(R.id.number1Text);
+        number2Text = findViewById(R.id.number2Text);
+        resultText = findViewById(R.id.resultText);
     }
-    public void sum(View view){
-       if (number1Text.getText().toString().matches("") || number2Text.getText().toString().matches("")){
-           resultText.setText("Enter Number!");
-       }else {
-           int number1 = Integer.parseInt(number1Text.getText().toString());
-           int number2 = Integer.parseInt(number2Text.getText().toString());
-           int result = number1+number2;
-           resultText.setText("Result: " + result);
-       }
 
+    public void sum(View view){
+        if (number1Text.getText().toString().isEmpty() || number2Text.getText().toString().isEmpty()){
+            resultText.setText(R.string.enter_number);
+        } else {
+            int number1 = Integer.parseInt(number1Text.getText().toString());
+            int number2 = Integer.parseInt(number2Text.getText().toString());
+            int result = number1 + number2;
+            resultText.setText(getString(R.string.result, result));
+        }
     }
+
     public void deduct(View view) {
-        if (number1Text.getText().toString().matches("") || number2Text.getText().toString().matches("")){
-            resultText.setText("Enter Number!");
-        }else {
+        if (number1Text.getText().toString().isEmpty() || number2Text.getText().toString().isEmpty()){
+            resultText.setText(R.string.enter_number);
+        } else {
             int number1 = Integer.parseInt(number1Text.getText().toString());
             int number2 = Integer.parseInt(number2Text.getText().toString());
-            int result = number1-number2;
-            resultText.setText("Result: " + result);
+            int result = number1 - number2;
+            resultText.setText(getString(R.string.result, result));
         }
     }
+
     public void multiply(View view){
-        if (number1Text.getText().toString().matches("") || number2Text.getText().toString().matches("")){
-            resultText.setText("Enter Number!");
-        }else {
+        if (number1Text.getText().toString().isEmpty() || number2Text.getText().toString().isEmpty()){
+            resultText.setText(R.string.enter_number);
+        } else {
             int number1 = Integer.parseInt(number1Text.getText().toString());
             int number2 = Integer.parseInt(number2Text.getText().toString());
-            int result = number1*number2;
-            resultText.setText("Result: " + result);
+            int result = number1 * number2;
+            resultText.setText(getString(R.string.result, result));
         }
     }
+
     public void divide(View view){
-        if (number1Text.getText().toString().matches("") || number2Text.getText().toString().matches("")){
-            resultText.setText("Enter Number!");
-        }else {
+        if (number1Text.getText().toString().isEmpty() || number2Text.getText().toString().isEmpty()){
+            resultText.setText(R.string.enter_number);
+        } else {
             int number1 = Integer.parseInt(number1Text.getText().toString());
             int number2 = Integer.parseInt(number2Text.getText().toString());
-            int result = number1/number2;
-            resultText.setText("Result: " + result);
+            if (number2 == 0) {
+                resultText.setText(R.string.divide_by_zero);
+            } else {
+                int result = number1 / number2;
+                resultText.setText(getString(R.string.result, result));
+            }
         }
     }
 }
